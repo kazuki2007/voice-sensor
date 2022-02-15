bbffea0ad68b2e5acd035642075617790532a9d9
kazuki2007 <95865549+kazuki2007@users.noreply.github.com>
2022-02-15 23:19:29 +0900

    1,停止・再開機能の追加   2,LEDを片側消す方式に変更

--- main.ts
+++ main.ts
@@ -2,15 +2,19 @@ let _1 = 0
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         _1 = 1
-    } else if (_1 == 1) {
+    } else if (input.buttonIsPressed(Button.B)) {
+        _1 = 0
+    }
+    if (_1 == 1) {
         maqueen.motorStop(maqueen.Motors.All)
     } else {
         if (input.soundLevel() > 95) {
             maqueen.motorStop(maqueen.Motors.All)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         } else {
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
         }
     }
