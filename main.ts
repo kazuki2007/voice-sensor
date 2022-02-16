9e73e84908eecb8215ef4a04e9cacb8bcf6759bf
kazuki2007 <95865549+kazuki2007@users.noreply.github.com>
2022-02-16 21:32:56 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,10 +9,18 @@ basic.forever(function () {
         maqueen.motorStop(maqueen.Motors.All)
     } else {
         if (input.soundLevel() > 95) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                . . # . .
+                . . . . .
+                . . # . .
+                `)
             maqueen.motorStop(maqueen.Motors.All)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
             maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         } else {
+            basic.clearScreen()
             maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
             maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
